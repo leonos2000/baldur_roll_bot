c39f4aa8ff5287ee0490ff37db918624050a959d
Leon Czajka <leonpiotrczajka@gmail.com>
2020-06-20 14:39:38 +0200

    KURWA

--- baldur.py
+++ baldur.py
@@ -1,44 +1,72 @@
-import win32api, time, os, keyboard, win32con, pytesseract, pyscreenshot as sc
+import win32api, win32con, win32gui
+import time
+import os
+import keyboard
+import pytesseract
+import pyscreenshot as sc
 from pynput.mouse import Button, Controller
 
-keyboard.wait('a')
+class Baldur:
 
-def roll():
-    win32api.SetCursorPos((1200,1000))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN|win32con.MOUSEEVENTF_ABSOLUTE,0,0)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP|win32con.MOUSEEVENTF_ABSOLUTE,0,0)
-    time.sleep(0.1)
+    def __init__(self, bestRoll = 0):
+        self.bestRoll = bestRoll
 
-def save():
-    time.sleep(1)
-    win32api.SetCursorPos((900,1000))
-    time.sleep(0.2)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN|win32con.MOUSEEVENTF_ABSOLUTE,0,0)
-    time.sleep(0.5)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP|win32con.MOUSEEVENTF_ABSOLUTE,0,0)
-    time.sleep(1)
+    def roll(self, wndPosize):
+        win32api.SetCursorPos((wndPosize[0] + 440, wndPosize[1] + 695))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_ABSOLUTE, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_ABSOLUTE, 0, 0)
+        time.sleep(0.1)
 
-def ocr():
-    img = sc.grab(bbox=(1120, 920, 1200, 940), backend="mss", childprocess=False)  # X1,Y1,X2,Y2
+        x = wndPosize[0] + 360
+        y = wndPosize[1] + 615
+        img = sc.grab(bbox=(x, y, x + 80, y + 20), backend="mss", childprocess=False)  # X1,Y1,X2,Y2
 
-    custom_config = r'-c tessedit_char_whitelist=0123456789 --oem 0'
-    return pytesseract.image_to_string(img, config=custom_config)   
+        img.save('box.png')
+
+        custom_config = r'-c tessedit_char_whitelist=0123456789 --oem 0'
+        return pytesseract.image_to_string(img, config=custom_config)   
+
+    def save(self, wndPosize):
+        time.sleep(1)
+        win32api.SetCursorPos((wndPosize[0] + 140, wndPosize[1] + 695))
+        time.sleep(0.2)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_ABSOLUTE, 0, 0)
+        time.sleep(0.5)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_ABSOLUTE, 0, 0)
+        time.sleep(1)
+        
+
+class Wnd:
+    def getWindowCallback(self, hwnd, extra):
+        if "Baldur" in win32gui.GetWindowText(hwnd):
+            rect = win32gui.GetWindowRect(hwnd)
+            self.x = rect[0]
+            self.y = rect[1]
+            self.w = rect[2] - self.x
+            self.h = rect[3] - self.y
+
+    def __init__(self):
+        win32gui.EnumWindows(self.getWindowCallback, None)
+
+    def refresh(self):
+        win32gui.EnumWindows(self.getWindowCallback, None)
+
+    def getPosize(self):
+        return (self.x, self.y, self.w, self.h)
 
 
-bestRoll = 9
+wnd = Wnd()
+bg = Baldur()
 
 keyboard.add_hotkey('q', lambda: os._exit(0))
+keyboard.wait('a')
 
 while 1:
-    roll()
+    currRoll = int(bg.roll(wnd.getPosize()))
 
-    # time.sleep(0.05)
+    print("Current roll: ", currRoll, ", best roll: ", bg.bestRoll)
 
-    currRoll = int(ocr())
-
-    print("Current roll: ", currRoll, ", best roll: ", bestRoll)
-
-    if currRoll > bestRoll:
-        save()
-        bestRoll = currRoll
-        print("******************* NEW BEST ROLL! : ", bestRoll, " **********************")
+    if currRoll > bg.bestRoll:
+        bg.save(wnd.getPosize())
+        bg.bestRoll = currRoll
+        print("******************* NEW BEST ROLL! : ", bg.bestRoll, " **********************")
